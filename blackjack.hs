8267118b339afd203a357151adf57ed4050a9720
Maniz Shrestha <mashres15@earlham.edu>
2017-10-29 01:14:00 -0400

    Updated blackjack

--- blackjack.hs
+++ blackjack.hs
@@ -35,7 +35,7 @@ splitShuffle deck = shuffle deck1 deck2
 shuffleDeck :: Deck -> Deck
 shuffleDeck deck = do
    -- n <- randomRIO(3,8) :: IO Int
-   d <- (iterate splitShuffle deck) !! (4)
+   d <- (iterate splitShuffle deck) !! (3)
    return d
 
 -------------------------------------------------------------------------------
@@ -75,20 +75,46 @@ doPlayerTurn (player, deck) = do
       
       if (scoreOver21 player')
           then do 
+              putStrLn "\n****************************"
               putStr "You went over 21. You lose!\n"
               exitSuccess
           else do
-              output <- (doPlayerTurn (player', deck'))
-              return output
+              gameStatus <- (doPlayerTurn (player', deck'))
+              return gameStatus
 
     else do
-      putStrLn "\nYou chose to stand..."
+      putStrLn "\n--------------------------------"
+      putStrLn "You chose to stand..."
+      putStrLn "--------------------------------"
       return (player, deck)
       
+doDealerTurn :: (Player, Deck) -> IO (Player, Deck)
+doDealerTurn (dealer, deck) = do
+    
+    if (highScore dealer < 17)
+    then do
+      putStrLn "\nDealer hits..."
+      let (dealer', deck') = hit (dealer, deck)
+      
+      putStr "Dealer's hand: "
+      print dealer'
+      putStr "Dealer's Score: "
+      print (highScore dealer')
+      
+      if (dealerOver21 dealer')
+          then do 
+              putStrLn "\n****************************"
+              putStr "Dealer went over 21. You Win!\n"
+              exitSuccess
+          else do
+              gameStatus <- (doDealerTurn (dealer', deck'))
+              return gameStatus
+      
+    else return (dealer, deck)
 
-
+-- Card Value when Ace ==1
 cardValue :: PlayingCards -> Int
-cardValue (Card Ace _) = 11
+cardValue (Card Ace _) = 1
 cardValue (Card King _) = 10
 cardValue (Card Queen _) = 10
 cardValue (Card Jack _) = 10
@@ -102,17 +128,98 @@ cardValue (Card Eight _) = 8
 cardValue (Card Nine _) = 9
 cardValue (Card Ten _) = 10
 
-handScore :: Player -> Int
-handScore player = sum $ map cardValue player
+-- Card Value when Ace ==1
+cardValue2 :: PlayingCards -> Int
+cardValue2 (Card Ace _) = 11
+cardValue2 (Card King _) = 10
+cardValue2 (Card Queen _) = 10
+cardValue2 (Card Jack _) = 10
+cardValue2 (Card Two _) = 2
+cardValue2 (Card Three _) = 3
+cardValue2 (Card Four _) = 4
+cardValue2 (Card Five _) = 5
+cardValue2 (Card Six _) = 6
+cardValue2 (Card Seven _) = 7
+cardValue2 (Card Eight _) = 8
+cardValue2 (Card Nine _) = 9
+cardValue2 (Card Ten _) = 10
+
+lowScore :: Player -> Int
+lowScore player = sum $ map cardValue player
+
+highScore :: Player -> Int
+highScore player = sum $ map cardValue player
 
 scoreOver21 :: Player -> Bool
 scoreOver21 player =
-  handScore player > 21
+  lowScore player > 21
+
+dealerOver21 :: Player -> Bool
+dealerOver21 player =
+  highScore player > 21
   
+declareWinner:: Player -> Player -> IO()
+declareWinner player dealer = do
+    putStr "\nYour cards: "
+    print player
+    putStr "Your score: "
+    print (highScore player)
+    
+    
+    putStr "\nDealer cards: "
+    print dealer
+    putStr "Your score: "
+    print (highScore dealer)
+    
+    putStrLn "\n**************************"
+    if (highScore player) > (highScore dealer)
+        then putStrLn "Congrats! You Win"
+        else if (highScore player) == (highScore dealer)
+            then putStrLn "The game is a tie."
+            else putStrLn "You Lose!"
+
+doTurns:: (Player, Player, Deck) -> IO()
+doTurns (player, dealer, deck) = do
+    ------------------------------------------------------------------------------------
+  -- Player turn
+  ------------------------------------------------------------------------------------
+  putStrLn "\n--------------------------------"
+  putStrLn "Your Turn..."
+  putStrLn "--------------------------------"
+  (player', deck') <- doPlayerTurn (player, deck)
+  
+  ------------------------------------------------------------------------------------
+  -- Dealer turn
+  ------------------------------------------------------------------------------------
+  putStrLn "\n--------------------------------"
+  putStrLn "Dealer's Turn..."
+  putStrLn "--------------------------------\n"
+  putStr "Your cards: "
+  print player'
+  
+  putStr "Dealer's hand: "
+  print dealer
+  (dealer', deck'') <- doDealerTurn (dealer, deck')
+  putStrLn "\n--------------------------------"
+  putStrLn "Dealer Stands... "
+  putStrLn "--------------------------------"
+  ------------------------------------------------------------------------------------
+  -- Dealer turn Winner
+  ------------------------------------------------------------------------------------
+  
+  declareWinner player' dealer'
+  
+  putStrLn "\n************************"
+  putStrLn "End of Game \n"
+
 main :: IO ()
 main = do
   putStrLn "Welcome to BlackJack"
   putStrLn "---------------------------"
+  
+  ------------------------------------------------------------------------------------
+  -- Shuffle, deal and show Cards
+  ------------------------------------------------------------------------------------
   putStrLn "\nShuffling cards..."
   let deck = shuffleDeck newDeck
 
@@ -125,19 +232,8 @@ main = do
   print (player)
 
   putStr "Dealer's cards: "
-  print (getDealerFirstCard(getDealerCards game_tuple), "Hidden Card")
-
-  ------------------------------------------------------------------------------------
-  -- Player turn
-  ------------------------------------------------------------------------------------
-  (player', deck') <- doPlayerTurn (player, deck)
+  putStrLn "\n--------------------------------"
+  let dealer = getDealerCards game_tuple
+  print (getDealerFirstCard(dealer), "Hidden Card")
   
-  ------------------------------------------------------------------------------------
-  -- Dealer turn
-  ------------------------------------------------------------------------------------
-  
-  
-  
-  putStr "End "
-  
-
+  doTurns (player, dealer, deck)
